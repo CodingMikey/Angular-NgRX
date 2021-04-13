25164a0c85d42c72256dfdc1593a826a3e184231
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-04-13 18:47:01 -0500

    creating-user-reducer-&-subscribing

--- APM-Demo0/src/app/user/user.module.ts
+++ APM-Demo0/src/app/user/user.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { userReducer } from '../user/state/user.reducer';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -12,7 +14,8 @@ const userRoutes: Routes = [
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild(userRoutes)
+    RouterModule.forChild(userRoutes),
+    StoreModule.forFeature('users', userReducer)
   ],
   declarations: [
     LoginComponent
