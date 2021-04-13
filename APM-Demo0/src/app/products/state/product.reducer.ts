85aefaad9383abca3b893f974f768ef0519ba646
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-04-13 17:57:29 -0500

    subscribing-to-store-to-get-statechanges

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -5,7 +5,7 @@ import { createAction, createReducer, on } from "@ngrx/store";
 export const productReducer = createReducer(
     { showProductCode: true },
     on(createAction('[Product] Toggle Product Code'), state => {
-        console.log('origina state: ' + JSON.stringify(state));
+        
         return {
             ...state,
             showProductCode: !state.showProductCode
